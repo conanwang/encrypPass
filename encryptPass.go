073057deee9223eb7039f95035fb58b8aba3802a
conanwang <superdian520@163.com>
2021-06-26 13:41:11 +0800

    Update encryptPass.go

--- encryptPass.go
+++ encryptPass.go
@@ -8,27 +8,27 @@ import (
 )
 
 
-//aes���ܺ�����orig����Ϊ�����ַ�����keyΪ����ʱʹ�õ���Կ
+//aes加密函数，orig参数为密文字符串，key为加密时使用的密钥
 func AesEncrypt(orig string, key string) string {
-	// ת���ֽ�����
+	// 转成字节数组
 	origData := []byte(orig)
 	k := []byte(key)
-	// ������Կ
+	// 分组秘钥
 	block, _ := aes.NewCipher(k)
-	// ��ȡ��Կ��ĳ���
+	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
-	// ��ȫ��
+	// 补全码
 	origData = PKCS7Padding(origData, blockSize)
-	// ����ģʽ������cbc����
+	// 加密模式，采用cbc加密
 	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
-	// ��������
+	// 创建数组
 	cryted := make([]byte, len(origData))
-	// ����
+	// 加密
 	blockMode.CryptBlocks(cryted, origData)
 	return base64.StdEncoding.EncodeToString(cryted)
 }
 
-//����
+//补码
 func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
 	padding := blocksize - len(ciphertext)%blocksize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
@@ -36,29 +36,29 @@ func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
 }
 
 
-//ȥ��
+//去码
 func PKCS7UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
 	return origData[:(length - unpadding)]
 }
 
-//aes���ܺ�����cryted����Ϊ�����ַ�����keyΪ����ʱʹ�õ���Կ
+//aes解密函数，cryted参数为密文字符串，key为加密时使用的密钥
 func AesDecrypt(cryted string, key string) string {
-	// ת���ֽ�����
+	// 转成字节数组
 	crytedByte, _ := base64.StdEncoding.DecodeString(cryted)
 	k := []byte(key)
-	// ������Կ
+	// 分组秘钥
 	block, _ := aes.NewCipher(k)
-	// ��ȡ��Կ��ĳ���
+	// 获取秘钥块的长度
 	blockSize := block.BlockSize()
-	// ����ģʽ
+	// 加密模式
 	blockMode := cipher.NewCBCDecrypter(block, k[:blockSize])
-	// ��������
+	// 创建数组
 	orig := make([]byte, len(crytedByte))
-	// ����
+	// 解密
 	blockMode.CryptBlocks(orig, crytedByte)
-	// ȥ��ȫ��
+	// 去补全码
 	orig = PKCS7UnPadding(orig)
 	return string(orig)
 }
